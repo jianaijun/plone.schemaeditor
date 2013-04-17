10703e8b5b3f1f8324e14d8d4d723ef52088abe7
Steve McMahon <steve@dcn.org>
2013-04-17 09:01:01 -0700

    Add unload protection

--- plone/schemaeditor/browser/model/modeleditor.js
+++ plone/schemaeditor/browser/model/modeleditor.js
@@ -1,12 +1,15 @@
 /*jslint white: true, onevar: true, undef: true, newcap: true, nomen: true,
   plusplus: true, bitwise: true, regexp: false, indent: 4 */
 
-/*globals jQuery */
+/*globals jQuery, form_modified_message, ajax_noresponse_message */
+
+/* global message strings are from jsvariables.py in CMFPlone */
 
 jQuery(function ($) {
     var editor = ace.edit("modelEditor"),
         session = editor.getSession(),
-        myform = $("#saveform");
+        myform = $("#saveform"),
+        doc_changed = false;
 
     // editor tuneup
     editor.setTheme("ace/theme/textmate");
@@ -27,10 +30,21 @@ jQuery(function ($) {
     // enable save submit button on change
     session.on('change', function(e) {
         $('#saveform :submit').removeAttr('disabled');
+        doc_changed = true;
+    });
+
+    // unload protection
+    // See http://dev.w3.org/html5/spec-LC/history.html#unloading-documents
+    $(window).on("beforeunload", function(event) {
+        if (doc_changed) {
+            return form_modified_message;
+        } else {
+            event.returnValue = "";
+        }
     });
 
     // form submit handler; ajax posts data
-    myform.on( "submit", function( event ) {
+    myform.on("submit", function(event) {
         var action = myform.attr('action');
 
         // prevent real submit
@@ -44,17 +58,18 @@ jQuery(function ($) {
             if (rez.success) {
                 var messagespan = $("#messagespan");
 
+                doc_changed = false;
                 // disable save button
                 $('#saveform :submit').attr('disabled', 'disabled');
                 messagespan.html(rez.message);
                 messagespan.show().fadeOut(1000);
-
-
             } else {
                 alert(rez.message);
             }
         })
-        .fail(function() { alert("No response from server; not saved."); });
+        .fail(function() {
+            alert(ajax_noresponse_message);
+        });
 
     });
 });
